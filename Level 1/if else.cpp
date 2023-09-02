a884921308d7803be21db78d805f0690f78046d9
Lucky <luckykashyap3430@gmail.com>
2023-09-02 18:17:30 +0530

    conditional

---
+++ Level 1/if else.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+#define AUTHOR "Divyanshu"
+
+int main(){
+
+    int side=4;
+
+    if(side==4){
+        cout<<"Triangle"<<endl;
+    }
+    else{
+        cout<<"Not Triangle"<<endl;
+    }
+
+    return 0;
+}
