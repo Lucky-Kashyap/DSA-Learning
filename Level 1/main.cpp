e591f19c5e4195903c4326008eeb743dea6cb9b6
Lucky <luckykashyap3430@gmail.com>
2023-09-02 16:15:04 +0530

    use macros

--- Level 1/main.cpp
+++ Level 1/main.cpp
@@ -2,9 +2,17 @@
 
 using namespace std;
 
+#define AUTHOR "Divyanshu"
+
 int main(){
 
     cout<<"Level 1 Started..!"<<endl;
+
+    cout<<"Written By : "<<AUTHOR<<endl;
+    cout<<"Written By : "<<AUTHOR<<endl;
+    cout<<"Written By : "<<AUTHOR<<endl;
+    cout<<"Written By : "<<AUTHOR<<endl;
+    cout<<"Written By : "<<AUTHOR<<endl;
 
     return 0;
 }
