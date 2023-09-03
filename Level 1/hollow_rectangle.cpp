34a2e24745a477c7a42afc4b01194579839a56f3
Lucky <luckykashyap3430@gmail.com>
2023-09-03 15:29:40 +0530

    creating hollow rectangle

---
+++ Level 1/hollow_rectangle.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // hollow pattern
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+
+            if (i == 1 || i == 4)
+            {
+                cout << "*";
+            }
+            else
+            {
+                if (j == 0 || j == 4)
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+}
