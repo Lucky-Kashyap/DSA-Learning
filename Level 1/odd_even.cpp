e6fca36ee1e2cbd56136fb471c12132364fd4c5d
Lucky <luckykashyap3430@gmail.com>
2023-09-03 14:03:18 +0530

    program for checking number is odd or even

---
+++ Level 1/odd_even.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"Enter the number you want check... :  "<<endl;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"Number is EVEN"<<endl;
+    }
+    else{
+        cout<<"Number is ODD"<<endl;
+    } 
+}
