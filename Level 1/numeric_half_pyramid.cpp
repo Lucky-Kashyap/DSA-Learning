c28d57720e691621be13cf52a3babf3715d712ad
Lucky <luckykashyap3430@gmail.com>
2023-09-04 14:06:05 +0530

    full pyramid pattern problem

--- Level 1/numeric_half_pyramid.cpp
+++ Level 1/numeric_half_pyramid.cpp
@@ -1,23 +1,17 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-    
+int main()
+{
 
-       for(int i=1;i<=5;i++){
-        for(int j=1;j<=i;j++){
-            
-            
-                cout<<j;
-        
-          
-           
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+
+            cout << j;
         }
-          cout<<endl;
-        
+        cout << endl;
     }
-
-
-
 }
