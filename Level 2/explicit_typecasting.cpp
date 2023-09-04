dc0a97ef1db42a0bdb5a68eded751adee6be7547
Lucky <luckykashyap3430@gmail.com>
2023-09-04 21:52:35 +0530

    float to char

--- Level 2/explicit_typecasting.cpp
+++ Level 2/explicit_typecasting.cpp
@@ -13,10 +13,22 @@ int main(){
 
    // double to int
 
-   double num1 = 3.14768;
-int num2 = 10.9;
+//    double num1 = 3.14768;
+// int num2 = 10.9;
 
-int res = (int)num1 + num2;
+// int res = (int)num1 + num2;
+
+// cout<<res<<endl;
+
+// float to char
+
+// float num1 = 67.8;
+// char res = num1;
+
+// cout<<res<<endl;
+
+float num1 = 67.8;
+char res = (char)num1;
 
 cout<<res<<endl;
 
