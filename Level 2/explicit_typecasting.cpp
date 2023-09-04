f87b7da21ae912dcdc677ee7011a8f3e3a2eda6e
Lucky <luckykashyap3430@gmail.com>
2023-09-04 21:49:35 +0530

    double to int

--- Level 2/explicit_typecasting.cpp
+++ Level 2/explicit_typecasting.cpp
@@ -3,13 +3,22 @@ using namespace std;
 
 int main(){
 
-int num1 = 10;
-float num2 = 10.9;
+// int num1 = 10;
+// float num2 = 10.9;
 
-float res = num1 + (int)num2;
+// float res = num1 + (int)num2;
+
+// cout<<res<<endl;
+   
+
+   // double to int
+
+   double num1 = 3.14768;
+int num2 = 10.9;
+
+int res = (int)num1 + num2;
 
 cout<<res<<endl;
-   
 
 
 }
