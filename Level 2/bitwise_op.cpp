96ce917e92c7909d770cceecb8dd388629eb0e1d
Lucky <luckykashyap3430@gmail.com>
2023-09-05 11:28:49 +0530

    bitwise not operator

--- Level 2/bitwise_op.cpp
+++ Level 2/bitwise_op.cpp
@@ -8,9 +8,11 @@ int main()
     int a = 2;
     int b=3;
     
-    cout<<(5&10)<<endl;
-    
-    cout<<(a&b)<<endl;
+    // cout<<(5&10)<<endl;
+
+    // cout<<(a&b)<<endl;
+
+    cout<<(~a)<<endl;
     
   
 
