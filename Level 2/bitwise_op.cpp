afe0d6676ee804a8354bdda94bd7c3b1dd05e747
Lucky <luckykashyap3430@gmail.com>
2023-09-05 15:07:47 +0530

    xor operator

--- Level 2/bitwise_op.cpp
+++ Level 2/bitwise_op.cpp
@@ -5,16 +5,21 @@ using namespace std;
 
 int main()
 {
-    int a = 2;
-    int b=3;
-    
+    // int a = 2;
+    // int b=3;
+
     // cout<<(5&10)<<endl;
 
     // cout<<(a&b)<<endl;
 
-    cout<<(~a)<<endl;
-    
-  
+    // cout<<(~a)<<endl;
+
+    // xor
+
+    int a = 5;
+    int b = 5;
+
+    cout << (a ^ b) << endl;
 
     return 0;
 }
