9d038af45db9ed7ccc903ed6e728b70422c41225
Lucky <luckykashyap3430@gmail.com>
2023-09-05 18:50:20 +0530

    add Two Numbers program using function concept by passing parameters

---
+++ Level 2/functions/addToNumber.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+int addToNum(int num1, int num2)
+{
+
+    int sum = num1 + num2;
+
+    return sum;
+}
+int main()
+{
+
+    int res = addToNum(45, 34);
+
+    cout << res;
+
+    return 0;
+}
