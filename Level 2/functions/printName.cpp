8efc1c1dd0e005559ccfcf31046535bb2d640181
Lucky <luckykashyap3430@gmail.com>
2023-09-05 18:29:35 +0530

    learning functions

---
+++ Level 2/functions/printName.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+using namespace std;
+
+void printName(string name)
+{
+
+    for (int i = 0; i < 5; i++)
+    {
+
+        cout << name << endl;
+    }
+}
+int main()
+{
+
+    printName("Lucky Kashyap");
+
+    return 0;
+}
