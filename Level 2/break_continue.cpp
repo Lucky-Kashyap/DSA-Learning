d2c710b7b17f3db7dc570f0918e17e31ad8fed36
Lucky <luckykashyap3430@gmail.com>
2023-09-05 17:20:21 +0530

    continue keyword iteration skip

--- Level 2/break_continue.cpp
+++ Level 2/break_continue.cpp
@@ -4,21 +4,27 @@ using namespace std;
 
 int main()
 {
-    
-    
-for(int i=0;i<5;i++){
-    if(i==3){
-        break;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     if (i == 3)
+    //     {
+    //         break;
+    //     }
+
+    //     cout << i << endl;
+    // }
+
+
+     for (int i = 0; i < 5; i++)
+    {
+        if (i == 3)
+        {
+            continue;
+        }
+
+        cout << i << endl;
     }
-    
-    cout<<i<<endl;
-}
-
-
-
-    
-    
-  
 
     return 0;
 }
