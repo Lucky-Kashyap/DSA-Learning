1464bbc5af3c87f870622f87e91aa7a14fdab986
Lucky <luckykashyap3430@gmail.com>
2023-09-05 11:26:58 +0530

    bitwise operator

---
+++ Level 2/bitwise_op.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    int b=3;
+    
+    cout<<(5&10)<<endl;
+    
+    cout<<(a&b)<<endl;
+    
+  
+
+    return 0;
+}
