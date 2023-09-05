d31a3d29b7ad39e874b6d34db90660014a2aedff
Lucky <luckykashyap3430@gmail.com>
2023-09-05 17:05:20 +0530

    play with pre post operator

--- Level 2/pre_post_operator.cpp
+++ Level 2/pre_post_operator.cpp
@@ -5,37 +5,38 @@ using namespace std;
 
 int main()
 {
-    
-    
-// int a = 10;
 
-// cout<<a++<<endl;
-// cout<<a<<endl;
+    // int a = 10;
 
-// cout<<(++a)<<endl;
+    // cout<<a++<<endl;
+    // cout<<a<<endl;
 
-// cout<<a;
-    
-  
-      
-// int a =10;
+    // cout<<(++a)<<endl;
 
-// cout<<(--a)<<endl;
+    // cout<<a;
 
-// cout<<a;
+    // int a =10;
 
-// cout<<(a--)*10<<endl;
+    // cout<<(--a)<<endl;
 
-// cout<<a;
+    // cout<<a;
 
-int a =10;
+    // cout<<(a--)*10<<endl;
 
-cout<<(++a)*10<<endl;
-cout<<(a++)*10<<endl;
+    // cout<<a;
 
-cout<<a;
+    // int a =10;
 
+    // cout<<(++a)*10<<endl;
+    // cout<<(a++)*10<<endl;
 
+    // cout<<a;
+
+    int a = 10;
+
+    cout << (++a) * (a++) << endl;
+
+    cout << a;
 
     return 0;
 }
