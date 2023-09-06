194be9777252ed2ea257cee935530dcaa3ffd224
Lucky <luckykashyap3430@gmail.com>
2023-09-06 14:42:18 +0530

    add three numbers program

---
+++ Level 2/functions/add_Three_Num.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+void sum(int a,int b,int c){
+    cout<<"Sum is : "<<a+b+c<<endl;
+}
+
+
+int main()
+{
+    cout<<"Main Function"<<endl;
+    
+    sum(9,6,3);
+    
+
+    return 0;
+}
