1f681582e018e131f69f7ec9cabac0155d8ab22c
Lucky <luckykashyap3430@gmail.com>
2023-09-06 15:05:55 +0530

    use inbuilt method for finding maximum number

--- Level 2/functions/find_max.cpp
+++ Level 2/functions/find_max.cpp
@@ -13,7 +13,7 @@ int max(int a,int b,int c){
                 return b;
             }
         }
-        else if(b>c){
+        else if(b>c){ 
             return b;
         }
         else{
@@ -21,13 +21,23 @@ int max(int a,int b,int c){
         }
 }
 
+void maximum(int num1,int num2,int num3){
+    int ans = max(num1,num2);
+    int final = max(ans,num3);
+
+    cout<<"Maximum number is : "<<final<<endl;
+}
 int main()
 {
     cout<<"Main Function"<<endl;
     
-    int res =max(89,13,23);
+    // int res =max(89,13,23);
     
 
-    cout<<"Maximum no is : "<<res<<endl;
+    // cout<<"Maximum no is : "<<res<<endl;
+
+    maximum(3,3,2);
+
+    
     return 0;
 }
