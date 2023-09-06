23112428a67804e45e25512c6116c8e19a8afe5e
Lucky <luckykashyap3430@gmail.com>
2023-09-06 17:39:09 +0530

    debigging exercises

---
+++ Level 2/functions/debugging/1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	double width, height;
+	cin >> width >> height;
+	// int area = width * height;
+    double area = width * height;
+	cout << area << endl;
+	return 0;
+}
+
