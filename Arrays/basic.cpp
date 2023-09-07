c79de67d8f69e3a94650b7037707341d8f821b17
Lucky <luckykashyap3430@gmail.com>
2023-09-07 09:39:31 +0530

    creation of an array

--- Arrays/basic.cpp
+++ Arrays/basic.cpp
@@ -4,11 +4,20 @@ using namespace std;
 
 int main()
 {
+    // Creation of an array
+
+    
     int arr[10];
     arr[0] = 10;
+
+    char ch[102];
+    bool flages[100];
+    long num[250];
+    short data[40];
+
     
-    cout<<arr[0]<<endl;
-    cout<<arr<<endl;
+    // cout<<arr[0]<<endl;
+    // cout<<arr<<endl;
 
     return 0;
 }
