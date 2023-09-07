6c51569d5d6f04cec2aefbb2ca5bcd76a87ed610
Lucky <luckykashyap3430@gmail.com>
2023-09-07 13:46:52 +0530

    count zeros and ones in an array

---
+++ Arrays/count_zero_one.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+void countZeroOne(int arr[], int size)
+{
+    int countZero = 0;
+    int countOne = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == 0)
+        {
+            countZero++;
+        }
+        else
+        {
+            countOne++;
+        }
+    }
+
+    cout << "Zeros are " << countZero << " " << endl;
+    cout << "Ones are " << countOne << " " << endl;
+}
+
+int main()
+{
+
+    int arr[8] = {0, 1, 1, 1, 0, 0, 1, 1};
+
+    int size = 8;
+
+    countZeroOne(arr, size);
+
+    return 0;
+}
