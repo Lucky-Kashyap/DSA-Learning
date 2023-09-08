662a1a66c2b253c030c119b2bf6281243d34c698
Lucky <luckykashyap3430@gmail.com>
2023-09-08 14:51:07 +0530

    doubt class by lakshay bhaiya

---
+++ Level 2/Doubts/doubt_class.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+     int a;
+    long b;
+    
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+
+    
+    char ch='1';
+    char c=1;
+  
+  cout<<c<<endl;
+  cout<<ch<<endl;
+  
+
+    return 0;
+}
