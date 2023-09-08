66d28a930ec943e3ac36f086bdef05d038a1c06f
Lucky <luckykashyap3430@gmail.com>
2023-09-08 14:52:53 +0530

    right shift left shift

--- Level 2/Doubts/doubt_class.cpp
+++ Level 2/Doubts/doubt_class.cpp
@@ -5,20 +5,22 @@ using namespace std;
 int main()
 {
 
+    //      int a;
+    //     long b;
 
-     int a;
-    long b;
-    
-    cout<<sizeof(a)<<endl;
-    cout<<sizeof(b)<<endl;
+    //     cout<<sizeof(a)<<endl;
+    //     cout<<sizeof(b)<<endl;
 
-    
-    char ch='1';
-    char c=1;
-  
-  cout<<c<<endl;
-  cout<<ch<<endl;
-  
+    //     char ch='1';
+    //     char c=1;
+
+    //   cout<<c<<endl;
+    //   cout<<ch<<endl;
+
+    int a = -10;
+
+    cout << (a << 1) << endl;
+    cout << (a >> 1) << endl;
 
     return 0;
 }
