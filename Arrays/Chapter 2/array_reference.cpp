9127a079e4e1272c09ef51d3b7b57bcc37ba6dd7
Lucky <luckykashyap3430@gmail.com>
2023-09-09 13:49:36 +0530

    pass by reference value

---
+++ Arrays/Chapter 2/array_reference.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void solve(int &a){
+    a++;
+    cout<<a<<endl;
+}
+
+
+// Hello World11
+// 11
+
+
+int main()
+{
+    cout<<"Hello World";
+    
+    int a = 10;
+    
+    // a++;
+    
+    solve(a);
+    
+    // a++;
+    cout<<a<<endl;
+
+    return 0;
+}
