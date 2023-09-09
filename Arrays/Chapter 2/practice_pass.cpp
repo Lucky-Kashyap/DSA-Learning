1b76fb60d326d0f6f4adf9ea1074a2bc2d793177
Lucky <luckykashyap3430@gmail.com>
2023-09-09 14:30:57 +0530

    pass by reference

--- Arrays/Chapter 2/practice_pass.cpp
+++ Arrays/Chapter 2/practice_pass.cpp
@@ -2,25 +2,41 @@
 
 using namespace std;
 
-void solve(int a){
+void solve(int a)
+{
     a--;
-    a=a*10;
-    
-    cout<<a<<endl;
+    a = a * 10;
+
+    cout << a << endl;
 }
 
 int main()
 {
-    cout<<"Hello World";
-    
-    int a = 90;
-    
-    a++;
-    
-    solve(a);
-    
-    
-    cout<<a<<endl;
+    // cout<<"Hello World";
 
+    // int a = 90;
+
+    // a++;
+
+    // solve(a);
+
+    // cout<<a<<endl;
+
+    // int a = 50;
+
+    // cout<<a<<endl;
+    // int b = a;
+    // cout<<b<<endl;
+
+    int a = 50;
+
+    // cout<<a<<endl;
+    int &b = a;
+
+    a++;
+    b++;
+
+    cout << a << endl;
+    cout << b << endl;
     return 0;
 }
