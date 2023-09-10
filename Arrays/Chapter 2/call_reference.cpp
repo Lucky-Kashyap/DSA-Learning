ec9266868861ccef4507f587aaf369d5870af5e8
Lucky <luckykashyap3430@gmail.com>
2023-09-10 14:26:01 +0530

    pass value by reference using addressof & operator

--- Arrays/Chapter 2/call_reference.cpp
+++ Arrays/Chapter 2/call_reference.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
-int incrementBy1(int n)
+// int incrementBy1(int n)
+// {
+//     n = n + 1;
+
+//     return n;
+// }
+
+int incrementBy1(int &n)
 {
     n = n + 1;
 
@@ -20,10 +27,16 @@ int main()
     // cout << k << endl;
     // cout << c << endl;
 
+    // int n;
+    // cin >> n;
+    // n = incrementBy1(n);
+    // cout << n << endl;
+
     int n;
     cin >> n;
-    n = incrementBy1(n);
+    int res = incrementBy1(n);
     cout << n << endl;
+    cout << res << endl;
 
     return 0;
 }
