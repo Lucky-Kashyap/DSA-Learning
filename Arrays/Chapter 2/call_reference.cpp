c12909d4848b59e77282f2d7e2119a7a00cac4d6
Lucky <luckykashyap3430@gmail.com>
2023-09-10 14:18:26 +0530

    pass value to function

--- Arrays/Chapter 2/call_reference.cpp
+++ Arrays/Chapter 2/call_reference.cpp
@@ -2,16 +2,28 @@
 
 using namespace std;
 
+int incrementBy1(int n)
+{
+    n = n + 1;
+
+    return n;
+}
+
 int main()
 {
-    int a = 6;
+    // int a = 6;
 
-    int &k = a;
-    int &c = a;
+    // int &k = a;
+    // int &c = a;
 
-    cout << a << endl;
-    cout << k << endl;
-    cout << c << endl;
+    // cout << a << endl;
+    // cout << k << endl;
+    // cout << c << endl;
+
+    int n;
+    cin >> n;
+    n = incrementBy1(n);
+    cout << n << endl;
 
     return 0;
 }
