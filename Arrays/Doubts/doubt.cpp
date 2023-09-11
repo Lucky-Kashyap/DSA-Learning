6f817a0387356023f05f0193ad016a2a04789901
Lucky <luckykashyap3430@gmail.com>
2023-09-11 14:59:11 +0530

    doubt class

---
+++ Arrays/Doubts/doubt.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int arr[5] = {1,2,3,4,5};
+    
+    // char arr2[5];
+    
+    // // arr2=arr;
+    
+    // // cout<<arr<<endl;
+    
+    // cout<<arr2<<endl;
+
+
+    // char ch = 65;
+    
+    // cout<<ch<<endl;
+    
+    // int a = 2.9;
+    
+    // cout<<a<<endl;
+    
+    // int arr[5] = {1,2,3,4,5};
+    
+    // cout<<sizeof(arr)/sizeof(int)<<endl;
+    
+     char arr[5] = {'1','2','3','4','5'};
+    
+    cout<<sizeof(arr)/sizeof(char)<<endl;
+    
+    return 0;
+}
