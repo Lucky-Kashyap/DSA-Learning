e06716ffb46ebe9c5cf709ccf0ada64669858d9f
Lucky <luckykashyap3430@gmail.com>
2023-09-11 17:57:05 +0530

    vector front and end

--- Arrays/Vectors/vector_char.cpp
+++ Arrays/Vectors/vector_char.cpp
@@ -18,9 +18,18 @@ void printVector(vector<char> v)
 int main()
 {
 
+    // vector<char> arr;
+
+    // arr.push_back('A');
+
     vector<char> arr;
 
     arr.push_back('A');
+    arr.push_back('B');
+    arr.push_back('C');
+
+    cout << "FRont ELement : " << arr[0] << endl;
+    cout << "Ending ELement : " << arr[arr.size() - 1] << endl;
 
     printVector(arr);
 
