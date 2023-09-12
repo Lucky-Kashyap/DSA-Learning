0f63746729d296c059de31b466f2f2a4b3f7d671
Lucky <luckykashyap3430@gmail.com>
2023-09-12 18:45:58 +0530

    print Transpose

---
+++ Arrays/Level 3/print_transpose.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+void printTranspose(int arr[][3], int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            swap(arr[i][j], arr[j][i]);
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    int arr[3][3] = {
+        {10, 20, 7},
+        {2, 4, 8},
+        {10, 15, 10}};
+
+    int row = 3;
+    int col = 3;
+
+    printTranspose(arr, row, col);
+
+    return 0;
+}
