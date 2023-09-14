42764dd77a51227eed3f512b2f6168a308ae41e6
Lucky <luckykashyap3430@gmail.com>
2023-09-14 12:33:49 +0530

    find duplicate element

---
+++ Arrays/Searching/find_duplicate.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binarySearch(vector<int> arr, int target)
+{
+    // bool flag=false;
+    int start = 0;
+    int end = arr.size() - 1;
+    int mid = (start + end) / 2;
+    int ans = -1;
+
+    // while(start<=end){
+
+    //     if(arr[mid]==target){
+    //         flag=true;
+    //         return mid;
+    //     }
+    //     else if(arr[mid]<target){
+    //         start=mid+1;
+    //     }
+    //     else if(arr[mid]>target){
+    //         end=mid-1;
+    //     }
+    //     mid = (start+end)/2;
+    // }
+
+    for (int i = start; i <= end; i++)
+    {
+
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            end = mid - 1;
+
+            // flag=true;
+            // return mid;
+        }
+        else if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        else if (arr[mid] > target)
+        {
+            end = mid - 1;
+        }
+        mid = (start + end) / 2;
+    }
+
+    // return ans;
+
+    // if(ans==ans){
+    //     return ans;
+    // }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr;
+
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+    arr.push_back(30);
+    arr.push_back(30);
+    arr.push_back(30);
+    arr.push_back(70);
+    arr.push_back(80);
+
+    int target = 30;
+
+    int found = binarySearch(arr, target);
+
+    if (found == -1)
+    {
+        cout << "Target not found" << endl;
+    }
+    else
+    {
+        cout << "Target Found at index  : " << found << endl;
+    }
+
+    return 0;
+}
