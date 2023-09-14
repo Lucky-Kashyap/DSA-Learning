a78b6e2692451f6c21517bb7c0a21e8650786844
Lucky <luckykashyap3430@gmail.com>
2023-09-14 11:59:06 +0530

    binary search

---
+++ Arrays/Searching/binary_search.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool binarySearch(vector<int> arr, int target)
+{
+    bool flag = false;
+    int start = 0;
+    int end = arr.size() - 1;
+    // cout<<end;
+    int mid = (start + end) / 2;
+
+    while (start <= end)
+    {
+        mid = (start + end) / 2;
+
+        if (arr[mid] == target)
+        {
+            flag = true;
+            return mid;
+        }
+        if (arr[mid] < target)
+        {
+            start = mid + 1;
+        }
+        if (arr[mid] > target)
+        {
+            end = mid - 1;
+        }
+    }
+
+    // for(int i=0;i<end;i++){
+    //     if(arr[mid]==target){
+
+    //     }
+    // }
+
+    return flag;
+}
+
+int main()
+{
+    vector<int> arr;
+
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+    arr.push_back(40);
+    arr.push_back(50);
+    arr.push_back(60);
+    arr.push_back(70);
+    arr.push_back(80);
+
+    int target = 90;
+
+    bool found = binarySearch(arr, target);
+
+    found ? cout << "Target Found" : cout << "Target Not Found" << endl;
+
+    return 0;
+}
