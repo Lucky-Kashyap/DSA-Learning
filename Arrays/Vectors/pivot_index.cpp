fb04666d8293ba23a6562f20cf032b2f52df5753
Lucky <luckykashyap3430@gmail.com>
2023-09-16 16:05:28 +0530

    using prefix sum approach solve pivot index question

--- Arrays/Vectors/pivot_index.cpp
+++ Arrays/Vectors/pivot_index.cpp
@@ -25,15 +25,37 @@ int bruteforce(vector<int> &nums)
 }
 int prefixSumApproach(vector<int> &nums)
 {
+    vector<int> lSum(nums.size(), 0);
+    vector<int> rSum(nums.size(), 0);
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        lSum[i] = lSum[i - 1] + nums[i - 1];
+    }
+    for (int i = nums.size() - 2; i >= 0; i--)
+    {
+        rSum[i] = rSum[i + 1] + nums[i + 1];
+    }
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (lSum[i] == rSum[i])
+        {
+            return i;
+        }
+    }
+
     return -1;
 }
-int main(){
+int main()
+{
 
-    // vector <int>nums;
+    vector<int> nums{1, 7, 3, 6, 5, 6};
+
+    cout << "Pivot Index : " << prefixSumApproach(nums) << endl;
 
     // int pivotIndex(vector<int>& nums) {
 
     //  return bruteforce(nums);
     // }
-
 };
