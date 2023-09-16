0a196e413a7340f1227d7db321f569f1c1800d04
Lucky <luckykashyap3430@gmail.com>
2023-09-16 17:14:16 +0530

    missing element from an array with duplicates

---
+++ Arrays/Vectors/missing_element_duplicate.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+void missingElement(int arr[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        int index = abs(arr[i]);
+        if (arr[index - 1] > 0)
+        {
+            arr[index - 1] *= -1;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+
+    // int arr[]={1,2,3,4,5,2,3,2,3};
+    int arr[] = {1, 5, 3, 4, 3, 5, 6};
+    // 1 5 3 4 3 5 6
+
+    int size = sizeof(arr) / sizeof(int);
+
+    missingElement(arr, size);
+};
