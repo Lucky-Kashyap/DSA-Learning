675add0990f9cd4d0dd12ff8b93e31506df3682c
Lucky <luckykashyap3430@gmail.com>
2023-09-18 13:00:11 +0530

    taking user input

--- Arrays/Doubts/swap_two_num.cpp
+++ Arrays/Doubts/swap_two_num.cpp
@@ -5,14 +5,30 @@ using namespace std;
 
 int main()
 {
-    int a = 12;
-    int b = 15;
+    // int a = 12;
+    // int b = 15;
 
+    // int temp = a;
+    // a = b;
+    // b = temp;
+
+    // cout << a << b << endl;
+
+    int a;
+    int b;
+
+    cout << "Enter numbers : " << endl;
+
+    cin >> a;
+    cin >> b;
+
+    cout << "Before swapping number : " << a << "            " << b << endl;
     int temp = a;
     a = b;
     b = temp;
 
-    cout << a << b << endl;
+    cout << "Swap number : " << a << "          " << b << endl;
+    // cout<<a<<"          "<<b<<endl;
 
     return 0;
 }
