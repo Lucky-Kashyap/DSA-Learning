13be023faece1a8657ed8d67a06a4b2d8ab10a64
Lucky <luckykashyap3430@gmail.com>
2023-09-19 17:46:42 +0530

    square root of a number

---
+++ Arrays/Searching/find_square_root.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int mySqrt(int x)
+{
+    int s = 0;
+    int e = x;
+    long long int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while (s <= e)
+    {
+        // kya mid hi toh answer nahi
+        if (mid * mid == x)
+        {
+            return mid;
+        }
+        else if (mid * mid < x)
+        {
+            // ans store
+            // right me jao
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            // left me jana h
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    // vector<int>arr;
+
+    int num = 5;
+
+    cout << "Square of a " << num << " : " << mySqrt(num);
+
+    return 0;
+}
