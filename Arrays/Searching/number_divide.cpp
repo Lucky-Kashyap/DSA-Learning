d58d45b01a36a0254ef7d9be40950d018f06a6f5
Lucky <luckykashyap3430@gmail.com>
2023-09-21 14:28:56 +0530

    number divide

---
+++ Arrays/Searching/number_divide.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+int getQuotient(int divisor, int dividend)
+{
+    int s = 0;
+    int e = dividend;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+
+    while (s <= e)
+    {
+        if (mid * divisor == dividend)
+        {
+            return mid;
+        }
+
+        if (mid * divisor < dividend)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int divisor = 2;
+    int dividend = 20;
+
+    int ans = getQuotient(divisor, dividend);
+
+    cout << "Answer : " << ans << endl;
+
+    return 0;
+}
