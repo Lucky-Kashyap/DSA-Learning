b87592124a27d0d8bff865e86e0760ba1b939d10
Lucky <luckykashyap3430@gmail.com>
2023-09-22 22:18:57 +0530

    basic loop questions

---
+++ Level 1/basic_loop_qs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // ✅Print number from 280 to 250 with the help of /r loop.
+    for (int i = 280; i >= 250; i--)
+    {
+        cout << i << " ";
+    }
+
+    // ✅Print char from ‘A’ to ‘Z’ with the help of a for loop.
+    for (char i = 65; i <= 90; i++)
+    {
+        cout << i << " ";
+    }
+
+    // ✅Print char from ‘Z’ to ‘A’ with the help of a for loop.
+
+    for (char i = 90; i >= 65; i--)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
