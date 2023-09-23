c8a921bee0fc1c3981b6472e7f8aef5a86e5c9e5
Lucky <luckykashyap3430@gmail.com>
2023-09-23 14:31:38 +0530

    using function get the char length

--- Char Arrays & Strings/char_length.cpp
+++ Char Arrays & Strings/char_length.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 
 using namespace std;
-
-int main()
+int getLength(char ch[], int size)
 {
-    char ch[20];
+    int len = 0;
 
-    cin >> ch;
-
-    int count = 0;
     for (int i = 0; i < 20; i++)
     {
 
@@ -16,8 +12,33 @@ int main()
         {
             break;
         }
-        count++;
+        else
+        {
+            len++;
+        }
     }
+
+    return len;
+}
+
+int main()
+{
+    char ch[20];
+
+    cin >> ch;
+
+    int count = getLength(ch, 20);
+
+    // int count = 0;
+    // for (int i = 0; i < 20; i++)
+    // {
+
+    //     if (ch[i] == '\0')
+    //     {
+    //         break;
+    //     }
+    //     count++;
+    // }
     cout << "print the length ch " << count << endl;
 
     return 0;
