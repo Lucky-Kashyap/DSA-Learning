742fb5049f58fae738e6c76113d254a56d9cd91d
Lucky <luckykashyap3430@gmail.com>
2023-09-23 16:36:40 +0530

    try some builtin methods like at front back

--- Char Arrays & Strings/string_method.cpp
+++ Char Arrays & Strings/string_method.cpp
@@ -12,10 +12,17 @@ int main()
 
     // cout << "Length of String : " << name.length() << endl;
 
-    string name = "";
+    // string name = "";
 
-    cout << "Length of String : " << name.length() << endl;
-    cout << "String is empty or not  : " << name.empty() << endl;
+    // cout << "Length of String : " << name.length() << endl;
+    // cout << "String is empty or not  : " << name.empty() << endl;
+
+    string name = "lucky";
+
+    cout << "Character at position 0 is: " << name.at(0) << endl;
+
+    cout << "Front character of string is: " << name.front() << endl;
+    cout << "back character of string is: " << name.back() << endl;
 
     return 0;
 }
