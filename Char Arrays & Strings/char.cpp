b95e256b1036121b4120c871f94e19d89ef6f2d7
Lucky <luckykashyap3430@gmail.com>
2023-09-23 14:03:26 +0530

    char array does not read space

--- Char Arrays & Strings/char.cpp
+++ Char Arrays & Strings/char.cpp
@@ -4,20 +4,27 @@ using namespace std;
 
 int main()
 {
-    char ch[5];
+    // char ch[5];
+
+    // cin >> ch;
+
+    // // cout<<ch<<" ";
+    // cout << "printing the value of ch" << endl;
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout << "At index : " << i << "  " << ch[i] << " " << endl;
+    // }
+
+    // char temp = ch[6];
+    // int val = (int)temp;
+
+    // cout << "Printing null character asscii value : " << val << endl;
+
+    char ch[20];
 
     cin >> ch;
 
     // cout<<ch<<" ";
-    cout << "printing the value of ch" << endl;
-    for (int i = 0; i < 10; i++)
-    {
-        cout << "At index : " << i << "  " << ch[i] << " " << endl;
-    }
-
-    char temp = ch[6];
-    int val = (int)temp;
-
-    cout << "Printing null character asscii value : " << val << endl;
+    cout << "printing the value of ch " << ch << endl;
     return 0;
 }
