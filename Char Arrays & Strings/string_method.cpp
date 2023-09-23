e8f6bb2aac29bb4dacf42076c40d1b1a816922fa
Lucky <luckykashyap3430@gmail.com>
2023-09-23 16:39:54 +0530

    two string append

--- Char Arrays & Strings/string_method.cpp
+++ Char Arrays & Strings/string_method.cpp
@@ -17,12 +17,26 @@ int main()
     // cout << "Length of String : " << name.length() << endl;
     // cout << "String is empty or not  : " << name.empty() << endl;
 
-    string name = "lucky";
+    // string name = "lucky";
 
-    cout << "Character at position 0 is: " << name.at(0) << endl;
+    // cout << "Character at position 0 is: " << name.at(0) << endl;
 
-    cout << "Front character of string is: " << name.front() << endl;
-    cout << "back character of string is: " << name.back() << endl;
+    // cout << "Front character of string is: " << name.front() << endl;
+    // cout << "back character of string is: " << name.back() << endl;
+
+    string str1 = "Lucky";
+    string str2 = "Kashyap";
+
+    cout << "before appending" << endl;
+    cout << str1 << endl;
+    cout << str2 << endl;
+
+    // apend
+    str1.append(str2);
+
+    cout << "After appending" << endl;
+    cout << str1 << endl;
+    cout << str2 << endl;
 
     return 0;
 }
