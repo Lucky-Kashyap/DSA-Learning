54f8d241ee4078d220474202bd02798e327fd773
Lucky <luckykashyap3430@gmail.com>
2023-09-23 16:32:36 +0530

    check string is empty or not

--- Char Arrays & Strings/string_method.cpp
+++ Char Arrays & Strings/string_method.cpp
@@ -6,11 +6,16 @@ using namespace std;
 int main()
 {
     // char ch[20];
-    string name;
+    // string name;
 
-    getline(cin, name);
+    // getline(cin, name);
+
+    // cout << "Length of String : " << name.length() << endl;
+
+    string name = "";
 
     cout << "Length of String : " << name.length() << endl;
+    cout << "String is empty or not  : " << name.empty() << endl;
 
     return 0;
 }
