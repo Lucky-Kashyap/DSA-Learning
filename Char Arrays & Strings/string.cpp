7c9a7257f3a733bd112f3891586e546611378811
Lucky <luckykashyap3430@gmail.com>
2023-09-23 15:53:57 +0530

    string in cpp

---
+++ Char Arrays & Strings/string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    // char ch[20];
+    string ch;
+
+    // cin.getline(ch,100);
+    cin >> ch;
+
+    cout << "Original String : " << ch;
+
+    return 0;
+}
