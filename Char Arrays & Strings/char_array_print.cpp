9e21e368f7e2cff81697bb39ae4df7f28c704c43
Lucky <luckykashyap3430@gmail.com>
2023-09-25 17:15:44 +0530

    char array print

---
+++ Char Arrays & Strings/char_array_print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch[] = "abcdefghij";
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << ch[i] << " ";
+    }
+
+    return 0;
+}
