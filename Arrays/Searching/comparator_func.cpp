b7bea618ae3a80603511a1b08d313ba3b37cb368
Lucky <luckykashyap3430@gmail.com>
2023-09-30 18:26:18 +0530

    print vector of vector

--- Arrays/Searching/comparator_func.cpp
+++ Arrays/Searching/comparator_func.cpp
@@ -4,12 +4,31 @@
 
 using namespace std;
 
-void print(vector<int> v)
+// void print(vector<int> v)
+// {
+//     for (int i : v)
+//     {
+//         cout << i << " ";
+//     }
+// }
+
+void print(vector<vector<int>> &v)
 {
-    for (int i : v)
+    // for (int i : v)
+    // {
+    //     cout << i << " ";
+    // }
+
+    for (int i = 0; i < v.size(); i++)
     {
-        cout << i << " ";
+        // cout<< v[i] << " ";
+        vector<int> &temp = v[i];
+        int a = temp[0];
+        int b = temp[1];
+
+        cout << a << "   " << b << endl;
     }
+    cout << endl;
 }
 // increasing order sorting
 
@@ -20,18 +39,40 @@ void print(vector<int> v)
 
 // decreasing order sorting
 
-bool mycomp(int &a, int &b)
-{
-    return a > b;
-}
+// bool mycomp(int &a, int &b)
+// {
+//     return a > b;
+// }
+
 int main()
 {
 
     // vector<int> v = {5, 4, 3, 2, 1};
-    vector<int> v = {0, -12, -34, 23, 45, 23, 10, 98, 231};
+    // vector<int> v = {0, -12, -34, 23, 45, 23, 10, 98, 231};
 
     // sort(v.begin(), v.end());
-    sort(v.begin(), v.end(), mycomp);
+    // sort(v.begin(), v.end(), mycomp);
+
+    // print(v);
+
+    // vector of vector
+
+    vector<vector<int>> v;
+
+    int n;
+    cout << "Enter size : \n";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cout << "Enter a & b : " << endl;
+        cin >> a >> b;
+        vector<int> temp;
+        temp.push_back(a);
+        temp.push_back(b);
+        v.push_back(temp);
+    }
 
     print(v);
 
