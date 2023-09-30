06bcf97a5f84b32600f88bf280b7bc822ed8dc22
Lucky <luckykashyap3430@gmail.com>
2023-09-30 11:43:40 +0530

    decode message

---
+++ Char Arrays & Strings/decode_string.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string decodeMessage(string key, string message)
+{
+
+    // create mapping
+
+    char start = 'a';
+    char mapping[250] = {0};
+
+    for (auto ch : key)
+    {
+
+        if (ch != ' ' && mapping[ch] == 0)
+        {
+            mapping[ch] = start;
+            start++;
+        }
+    }
+
+    // use mapping
+
+    string ans;
+
+    for (auto ch : message)
+    {
+        if (ch == ' ')
+        {
+            ans.push_back(' ');
+        }
+        else
+        {
+            char decodedChar = mapping[ch];
+            ans.push_back(decodedChar);
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    string key = "the quick brown fox jumps over the lazy dog";
+    string message = "vkbs bs t suepuv";
+
+    cout << decodeMessage(key, message) << endl;
+    return 0;
+}
