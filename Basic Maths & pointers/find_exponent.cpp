d0d3f58eb5474f072b4f9d3048c8fd181f6db289
Lucky <luckykashyap3430@gmail.com>
2023-10-03 17:31:40 +0530

    find fast exponent

--- Basic Maths & pointers/find_exponent.cpp
+++ Basic Maths & pointers/find_exponent.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
 using namespace std;
+
+int fastExponentiation(int a, int b)
+{
+    int ans = 1;
+
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            // odd
+            ans = ans * a;
+        }
+
+        a = a * a;
+        b >>= 1;
+    }
+
+    return ans;
+}
 int slowExponentiation(int a, int b)
 {
     int ans = 1;
@@ -18,7 +37,8 @@ int main()
     int num = 5;
     int num2 = 5;
 
-    int res = slowExponentiation(num, num2);
+    // int res = slowExponentiation(num, num2);
+    int res = fastExponentiation(num, num2);
 
     cout << res << endl;
 
