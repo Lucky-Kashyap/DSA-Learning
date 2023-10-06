173448e47ad6a8d98557aec6597f83952072143c
Lucky <luckykashyap3430@gmail.com>
2023-10-06 16:12:55 +0530

    custom sort string using comparator

---
+++ Char Arrays & Strings/custom_sort_string.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+static string str;
+static bool compare(char char1, char char2)
+{
+
+       return (str.find(char1) < str.find(char2));
+}
+string customSortString(string order, string s)
+{
+
+    str = order;
+    sort(s.begin(), s.end(), compare);
+    return s;
+}
+
+int main()
+{
+    //   string order = "cba", s = "abcd";
+    string order = "cbafg", s = "abcd";
+    // Output: "cbad"
+
+    string res = customSortString(order, s);
+    cout << res << endl;
+
+    // Output: "cbad"
+
+    return 0;
+}
